29a1170011ad9aa5d8d78f2769827790730a2c2a
Ahmed786687 <ahmediqbalbhatti1@gmail.com>
2021-08-30 13:33:53 +0500

    almost there

--- DayFour.py
+++ DayFour.py
@@ -1,3 +1,4 @@
+import random
 # random_int = random.randint(0, 6)
 # print(random_int)
 #
@@ -12,19 +13,49 @@
 # else:
 #     print('Heads')
 
-# -----------------------DATA STRUCTURE (LIST)
-statesOfPakistan = ["Sindh", "Punjab", "Khybr Pakhtunekhwa", "Islamabad Capital Territory", "Gilgit-Baltistan",
-                    "Baluchistan", "Azad Jammu and Kashmir"]
+# # -----------------------DATA STRUCTURE (LIST)
+# statesOfPakistan = ["Sindh", "Punjab", "Khyber Pakhtunekhwa", "Islamabad Capital Territory", "Gilgit-Baltistan",
+#                     "Baluchistan", "Azad Jammu and Kashmir"]
 # statesOfPakistan.append("Azad Jammu and Kashmir")
 # statesOfPakistan.pop(6)
 # statesOfPakistan.insert(0, '34')
 # statesOfPakistan.reverse()
 # statesOfPakistan.sort()
 
-copyOfStates = statesOfPakistan.copy()
-print(copyOfStates)
-statesOfPakistan.clear()
-print(statesOfPakistan)
-print(copyOfStates.pop())
-print(copyOfStates)
+# copyOfStates = statesOfPakistan.copy()
+# print(copyOfStates)
+# statesOfPakistan.clear()
+# print(statesOfPakistan)
+# print(copyOfStates.pop())
+# print(copyOfStates)
+# # ----------------------------------------- SECOND CODING EXERCISE ---------------------------------------
+# print('WELCOME TO THE NAME SELECTOR FOR PAYING BILL')
+# print('Give me Names of Everybody, Separated by comma and space like (, )')
+# nameString = input()
+# names = nameString.split(", ")
+# length = names.__len__()
+# length = length-1
+# randomListNameSelector = random.randint(0, length)
+# print(f'{names[randomListNameSelector]} should pay the bill!')
+# # -----------------------------DATA STRUCTURE (Nested LIST)
+# statesOfPakistan = ["Sindh", "Punjab", "Khyber Pakhtunekhwa", "Islamabad Capital Territory", "Gilgit-Baltistan",
+#                     "Baluchistan", "Azad Jammu and Kashmir"]
+# numOfStates = len(statesOfPakistan)
+# print(statesOfPakistan[numOfStates-1])
+
+# dirtyDozen = ["Strawberries", "Spinach", "Apples", "Grapes", "Tomatoes", "Potatoes"]
+
+# fruits = ["Strawberries", "Apples", "Grapes"]
+# vegetables = ["Spinach", "Tomatoes", "Potatoes"]
+# dirtyDozen = [fruits, vegetables]
+# print(fruits)
+# print(vegetables)
+# print(dirtyDozen)
+# # ----------------------------------------- THIRD CODING EXERCISE ---------------------------------------
+print('welcome to the program which will mark a spot with an \'X\''.upper())
+row1 =["😆", "😂", "😚"]
+row2 =["😆", "😂", "😚"]
+row3 =["😆", "😂", "😚"]
+# print(f'    1     2     3 \n1 {row1}\n2 {row2}\n3 {row3}')
+map = [row1, row2, row3]
 
