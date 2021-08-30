305fb6b8b2dd33f74587e335de695c8b43d06389
Ahmed786687 <ahmediqbalbhatti1@gmail.com>
2021-08-30 19:12:50 +0500

    Complete Day Four

--- DayFour.py
+++ DayFour.py
@@ -1,6 +1,3 @@
-import math
-import random
-
 # random_int = random.randint(0, 6)
 # print(random_int)
 #
@@ -40,8 +37,8 @@ import random
 # randomListNameSelector = random.randint(0, length)
 # print(f'{names[randomListNameSelector]} should pay the bill!')
 # # -----------------------------DATA STRUCTURE (Nested LIST)
-# statesOfPakistan = ["Sindh", "Punjab", "Khyber Pakhtunekhwa", "Islamabad Capital Territory", "Gilgit-Baltistan",
-#                     "Baluchistan", "Azad Jammu and Kashmir"]
+# statesOfPakistan = ["Sindh", "Punjab", "Khyber Pakhtunekhwa", "Islamabad Capital Territory",
+#                     "Gilgit-Baltistan", "Baluchistan", "Azad Jammu and Kashmir"]
 # numOfStates = len(statesOfPakistan)
 # print(statesOfPakistan[numOfStates-1])
 
@@ -53,38 +50,108 @@ import random
 # print(fruits)
 # print(vegetables)
 # print(dirtyDozen)
-# # ----------------------------------------- THIRD CODING EXERCISE ---------------------------------------
-print('welcome to the program which will mark a spot with an \'X\''.upper())
-row1 = ["😆", "😂", "😚"]
-row2 = ["😆", "😂", "😚"]
-row3 = ["😆", "😂", "😚"]
-print(f'    1     2     3 \n1 {row1}\n2 {row2}\n3 {row3}')
-map = [row1, row2, row3]
-print(map)
-address = input('Where you want to place \"X\"')
-column = int(address[0])  # 2
-row = int(address[1])  # 3
-print(f'column {column} and row is {row}')
-# print(map[column-1])
-selectedRow = map[row - 1]
-selectedRow[column-1] = "X"
+# # # ----------------------------------------- THIRD CODING EXERCISE ---------------------------------------
+# print('welcome to the program which will mark a spot with an \'X\''.upper())
+# row1 = ["😆", "😂", "😚"]
+# row2 = ["😆", "😂", "😚"]
+# row3 = ["😆", "😂", "😚"]
+# print(f'    1     2     3 \n1 {row1}\n2 {row2}\n3 {row3}')
+# map = [row1, row2, row3]
+# print(map)
+# address = input('Where you want to place \"X\"')
+# column = int(address[0])  # 2
+# row = int(address[1])  # 3
+# # print(column {column} and row is {row}')
+# map[row-1][column-1]= "X"
+# # print(map[column-1])
+# # ==========Simple Method
+# # selectedRow = map[row - 1]
+# # selectedRow[column-1] = "X"
+#
+# # ==========Easy and Long Method
+# # if address == 11:
+# #     row1[0] = "X"
+# # elif address == 12:
+# #     row1[1] = "X"
+# # elif address == 13:
+# #     row1[2] = "X"
+# # elif address == 21:
+# #     row2[0] = "X"
+# # elif address == 22:
+# #     row2[1] = "X"
+# # elif address == 23:
+# #     row2[2] = "X"
+# # elif address == 31:
+# #     row3[0] = "X"
+# # elif address == 32:
+# #     row3[1] = "X"
+# # elif address == 33:
+# #     row3[2] = "X"
+# print(f'    1     2     3 \n1 {row1}\n2 {row2}\n3 {row3}')
+# ---------------------------------------------Day Four Project---------------------------
+import random
 
-# if address == 11:
-#     row1[0] = "X"
-# elif address == 12:
-#     row1[1] = "X"
-# elif address == 13:
-#     row1[2] = "X"
-# elif address == 21:
-#     row2[0] = "X"
-# elif address == 22:
-#     row2[1] = "X"
-# elif address == 23:
-#     row2[2] = "X"
-# elif address == 31:
-#     row3[0] = "X"
-# elif address == 32:
-#     row3[1] = "X"
-# elif address == 33:
-#     row3[2] = "X"
-print(f'    1     2     3 \n1 {row1}\n2 {row2}\n3 {row3}')
+print("WELCOME TO THE ROCK PAPER SCISSORS GAME")
+print('''What do you choose?
+      Type "0" for Rock,
+      Type "1" for Paper,
+      Type "2" for Scissors''')
+userInput = input()
+rock = """
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+"""
+paper = """
+     _______
+---'    ____)____
+           ______)
+          _______)
+         _______)
+---.__________)
+"""
+scissors = """
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+"""
+if userInput == "0":
+    print('You choose Rock.')
+    print(rock)
+elif userInput == "1":
+    print('You choose Paper.')
+    print(paper)
+elif userInput == "2":
+    print('You choose Scissors.')
+    print(scissors)
+computerChoose = str(random.randint(0, 2))
+if computerChoose == "0":
+    print('Computer choose Rock.')
+    print(rock)
+elif computerChoose == "1":
+    print('Computer choose Paper.')
+    print(paper)
+elif computerChoose == "2":
+    print('Computer choose Scissors.')
+    print(scissors)
+print('RESULT')
+if userInput == "0" and computerChoose == "2":
+    print('You WIN!')
+elif userInput == "1" and computerChoose == "0":
+    print('You WIN!')
+elif userInput == "2" and computerChoose == "1":
+    print('You WIN!')  # ends user wins
+elif userInput == "2" and computerChoose == "0":
+    print('COMPUTER WIN!')
+elif userInput == "0" and computerChoose == "1":
+    print('COMPUTER WIN!')
+elif userInput == "1" and computerChoose == "2":
+    print('COMPUTER WIN!')
+else:
+    print('DRAW!')
