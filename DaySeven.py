8fd4e1911f5807f39c2d29f69ade71722e0c4f0a
Ahmed786687 <ahmediqbalbhatti1@gmail.com>
2021-09-14 17:06:47 +0500

    Complete Day Seven

--- DaySeven.py
+++ DaySeven.py
@@ -1,113 +1,44 @@
 # Here I am going to Create function for Generating Random Word
 import random
-
-import DaySevenHangmanWords
-
-# wordList = ['HELLO', 'WORLD', 'PROPOSED', 'BEEKEEPING', 'LOVER']
-stages = ['''
-      _______
-     |/      |
-     |      (_)
-     |      \|/
-     |       |
-     |      / /
-     |
-     |___
-''',
-          '''
-      _______
-     |/      |
-     |      (_)
-     |      \|/
-     |       |
-     |      / /
-     |
-     |___
-''',
-          '''
-      _______
-     |/      |
-     |      (_)
-     |      \|/
-     |       |
-     |      
-     |
-     |___
-''',
-          '''
-      _______
-     |/      |
-     |      (_)
-     |      \|/
-     |       
-     |      
-     |
-     |___
-''',
-          '''
-      _______
-     |/      |
-     |      (_)
-     |      \|
-     |       
-     |      
-     |
-     |___
-''',
-          '''
-      _______
-     |/      |
-     |      (_)
-     |       |
-     |      
-     |      
-     |
-     |___
-''',
-          '''
-      _______
-     |/      |
-     |      (_)
-     |      
-     |      
-     |      
-     |
-     |___
-''',
-          '''
-      _______
-     |/      |
-     |      
-     |      
-     |      
-     |      
-     |
-     |___
-'''
-          ]
+from DaySevenHangmanArts import logo
+from DaySevenHangmanWords import wordList
+from DaySevenHangmanArts import stages
+print(logo)
+chosenWord = random.choice(wordList)
 dashedList = []
-chosenWord = random.choice(DaySevenHangmanWords.wordList)
 for n in range(len(chosenWord)):
     dashedList.append('_')
+
 print(dashedList)
-print(chosenWord)
+# print(chosenWord)
 is_game_ends = False
-lives = 6
+
+
+lives = len(stages)
 while not is_game_ends:
-    guess = input('Guess A Letter: ').upper()
+    guess = input('Guess A Letter: ').lower()
+    if guess in dashedList:
+        print('Already Guessed! Please Try Something Else')
     i = 0
     for char in chosenWord:
         if char == guess:
             dashedList[i] = char
-            print("Matched")
         i += 1
     if guess not in chosenWord:
-        print(stages[lives])
+        print('Chosen letter is not in the world! Please THINK and '
+              'Try Again')
         lives -= 1
         if lives == 0:
+            print(stages[lives])
             print('You LOSE')
             exit()
     print(dashedList)
     if '_' not in dashedList:
         is_game_ends = True
-        print('You WIN')
+        if lives == 6:
+            print('Excellent! You WIN')
+        elif lives == 4:
+            print('Good! You WIN')
+        else:
+            print('You WIN')
+    print(stages[lives])
